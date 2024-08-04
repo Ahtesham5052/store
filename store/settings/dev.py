ccc6ac30feb70214bdf8f995f29f3978d8ee32fb
Ahtesham5052 <muhammadahtesham5052@gmail.com>
2024-08-04 17:44:46 +0500

    done coding

--- store/settings/dev.py
+++ store/settings/dev.py
@@ -4,9 +4,14 @@ from .common import *
 SECRET_KEY = 'django-insecure-@t^wli-mv_h98)vptcmwcnw-*z(ayl61n3+)bs)*jgmf38b4t5'
 DEBUG = True
 
+import os
+import dj_database_url
 import pymysql
+
+# Install pymysql as MySQLdb
 pymysql.install_as_MySQLdb()
 
+# Default settings for database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -17,4 +22,9 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+# Check if DATABASE_URL is set and override default settings if it is
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
 
