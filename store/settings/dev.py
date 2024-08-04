28ebca2abf2946df625aea54032dcd570a29a2aa
Ahtesham5052 <muhammadahtesham5052@gmail.com>
2024-08-04 18:23:00 +0500

    Update mysqlclient version and settings

--- store/settings/dev.py
+++ store/settings/dev.py
@@ -23,5 +23,5 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
     DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
 
-SECRET_KEY = 'django-insecure-@t^wli-mv_h98)vptcmwcnw-*z(ayl61n3+)bs)*jgmf38b4t5'
+SECRET_KEY = 'uqx5(^x4_u_^0+p+^6^!efam8k8$=qtt9!ddovb2@8v87ni2ua'
 DEBUG = True
